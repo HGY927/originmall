51f0ae101791dbeb79b15ee35ae158566e05c078
HGY927 <1147424594@qq.com>
2022-11-16 10:25:38 +0800

    优化部分业务

--- originmall/controller/RegisterController.go
+++ originmall/controller/RegisterController.go
@@ -9,9 +9,9 @@ import (
 )
 
 func RegisterController(w http.ResponseWriter, r *http.Request) {
-	var userserver server.UserServer
+	var userserver *server.UserServer
 	json.NewDecoder(r.Body).Decode(&userserver)
-	str := utils.CheckField(&userserver)
+	str := utils.CheckField(userserver)
 	if str != "" {
 		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -23,6 +23,6 @@ func RegisterController(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(server.Register(&userserver))
+	json.NewEncoder(w).Encode(server.Register(userserver))
 
 }
